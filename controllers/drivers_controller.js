ea53eb7f6d3f5ae7fa3a1a10d45f2ccfd8661588
Peter Pham <phamhere@gmail.com>
2018-12-31 06:13:28 -0700

    added error handling middleware

--- controllers/drivers_controller.js
+++ controllers/drivers_controller.js
@@ -4,8 +4,10 @@ module.exports = {
   greeting(req, res) {
     res.send({ hi: "there" });
   },
-  create(req, res) {
+  create(req, res, next) {
     const driverProps = req.body;
-    Driver.create(driverProps).then(driver => res.send(driver));
+    Driver.create(driverProps)
+      .then(driver => res.send(driver))
+      .catch(next);
   }
 };
