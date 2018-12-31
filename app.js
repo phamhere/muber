775c030b18d04d2e485cc247003eb4984d80cfc5
Peter Pham <phamhere@gmail.com>
2018-12-31 06:02:03 -0700

    POST driver test implemented and test/dev servers separated

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes/routes");
 
 const app = express();
 
+if (process.env.NODE_ENV !== "test") {
+  mongoose.connect(
+    "mongodb://localhost/muber",
+    {
+      useNewUrlParser: true
+    }
+  );
+}
+
+app.use(express.json());
 routes(app);
 
 module.exports = app;
